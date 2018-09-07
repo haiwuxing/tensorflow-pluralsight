a04547a07fd186f4825355f94a6c0d014a031adf
Li Jian <lijian@hantek.com>
2018-09-07 16:58:06 +0800

    备份

--- Module 2 - Basic TF/demos/House_Price_Prediction.py
+++ Module 2 - Basic TF/demos/House_Price_Prediction.py
@@ -24,7 +24,7 @@ house_price = house_size * 100.0 + np.random.randint(low=20000, high=70000, size
 plt.plot(house_size, house_price, "bx") # bx = blue x
 plt.ylabel("Price")
 plt.xlabel("Size")
-plt.show()
+# plt.show()
 
 # you need to normalize values to prevent under/overflows.
 def normalize(array):
@@ -50,3 +50,68 @@ test_house_price_norm = normalize(test_house_price)
 # Set up the TensorFlow placholders that get updated as we descend down the gradient
 tf_house_size = tf.placeholder("float", name="house_size")
 tf_price = tf.placeholder("float", name="price")
+
+# Define the variables holding the size_factor and price we set during training.
+# We initialize them to some random values based on teh normal distribution.
+tf_size_factor = tf.Variable(np.random.randn(), name="size_factor")
+tf_price_offset = tf.Variable(np.random.randn(), name="price_offset")
+
+# 2. Define the operations for the predictiong values
+tf_price_pred = tf.add(tf.multiply(tf_size_factor, tf_house_size), tf_price_offset)
+
+# 3. Define the Loss Function (how much error) - Mean squared error
+tf_cost = tf.reduce_sum(tf.pow(tf_price_pred - tf_price, 2)) / (2 * num_train_samples)
+
+# Optimizer learning rate. The size of the steps down the gradient
+learning_rate = 0.1
+
+# 4. Define a Gradient descent optimizer that will minimize the loss defined in the operation "cost".
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(tf_cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph int he session
+with tf.Session() as sess:
+    sess.run(init)
+
+    # set how often to display training process and number of training iterations
+    display_every = 2
+    num_training_iter = 50
+
+    # keep iterating the training data
+    for iteration in range(num_training_iter):
+
+        # Fit all training data
+        for (x,y) in zip(train_house_size_norm, train_price_norm):
+            sess.run(optimizer, feed_dict={tf_house_size: x, tf_price: y})
+
+        # Display current status
+        if (iteration + 1) % display_every == 0:
+            c = sess.run(tf_cost, feed_dict={tf_house_size: train_house_size_norm, tf_price:train_price_norm})
+            print("iteration #:", '%04d' % (iteration + 1), "cost=", "{:.9f}".format(c), \
+                "size_factor=", sess.run(tf_size_factor), "price_offset=", sess.run(tf_price_offset))
+
+    print("Optimization Finished!")
+    training_cost = sess.run(tf_cost, feed_dict={tf_house_size: train_house_size_norm, tf_price: train_price_norm})
+    print("Trained cost=", training_cost, "size_factor=", sess.run(tf_size_factor), "price_offset=", sess.run(tf_price_offset), "\n")
+
+    train_house_size_mean = train_house_size.mean()
+    train_house_size_std = train_house_size.std()
+
+    train_price_mean = train_price.mean()
+    train_price_std = train_price.std()
+
+    # Plot the graph
+    plt.rcParams["figure.figsize"] = (10,8)
+    plt.figure()
+    plt.ylabel("Price")
+    plt.xlabel("Size (sq.ft)")
+    plt.plot(train_house_size,train_price, 'go', label='Training data')
+    plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
+    plt.plot(train_house_size_norm * train_house_size_std + train_house_size_mean,
+                (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset)) * train_price_std + train_price_mean,
+                label='Learned Regression')
+
+    plt.legend(loc='upper left')
+    plt.show()
